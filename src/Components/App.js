567426bbf398b9191948c324f2309a970b2f311b
Isaac <hawkins.isaac@gmail.com>
2017-10-25 19:46:23 -0500

    map dispatch to props and bind action creators

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+	constructor(props) {
+		super();
+		this.state = {
+			text: ''
+		};
+	}
+
+	addReminder() {
+		console.log('this', this);
+		this.props.addReminder(this.state.text);
+	}
+
 	render() {
 		return (
-			<div className="App">
-				<div className="title">
-					Reminder Pro
-				</div>
-	 			<div className="form-inline">
-					<div className="form-group">
-						<input 
-							className="form-control"
-							placeholder="I have to..."
+			<div className='App'>
+				<div className='title'>Reminder Pro</div>
+				<div className='form-inline'>
+					<div className='form-group'>
+						<input
+							className='form-control'
+							placeholder='I have to...'
+							onChange={event => this.setState({ text: event.target.value })}
 						/>
 					</div>
 					<button
-						type="button"
-						className="btn btn-success"
-					>Add Reminder</button>
+						type='button'
+						className='btn btn-success'
+						onClick={this.addReminder.bind(this)}
+					>
+						Add Reminder
+					</button>
 				</div>
 			</div>
-
-		)
+		);
 	}
 }
 
-export default App;
+
+export default connect(null, { addReminder })(App);
